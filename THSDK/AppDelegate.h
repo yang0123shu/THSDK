4fad4b954efa3f75a3eff950bbce063b1c5d5739
yang0123shu <yang0123shu@163.com>
2016-09-22 10:31:58 +0800

    Initial Commit

---
+++ THSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  THSDK
+//
+//  Created by 阳书成 on 16/9/22.
+//  Copyright © 2016年 阳书成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
